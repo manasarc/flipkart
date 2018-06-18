b705a67a9073ec480c3c0cd3f166fa8ed28e0035
TYSS <TYSS@TYSS-TR-0015>
2018-06-18 16:35:51 +0530

    1st commit

---
+++ flipkart/src/test/java/com/tyss/flipkart/Test1.java
@@ -0,0 +1,11 @@
+package com.tyss.flipkart;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test
+public void test1() {
+	Reporter.log("test1", true);
+}
+}
